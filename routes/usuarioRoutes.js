60d6725359b47271ceaee255446cd524ac8b9c1d
Juan Felipe Muñoz <juanfelipemh@gmail.com>
2023-06-12 14:24:51 -0500

    prueba

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -5,7 +5,7 @@ import  checkAuth  from "../middleware/authValidacion.js";
 
 const router = express.Router();
 
-router.post("/usuario/registrar", registroUsuario);
+router.post("/usuario/registrar",  registroUsuario);
 router.put("/usuario/modificar/:id", checkAuth, modificarUsuario)
 router.get("/usuario/", checkAuth, obtenerUsuario)
 
