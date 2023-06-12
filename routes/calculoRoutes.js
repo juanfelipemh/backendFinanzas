a56374b35ee92dd3c1c8f2fbbbcca12c0e670421
Juan Felipe Muñoz Hurtado <110547666+juanfelipemh@users.noreply.github.com>
2023-06-12 14:13:48 -0500

    Update calculoRoutes.js

--- routes/calculoRoutes.js
+++ routes/calculoRoutes.js
@@ -5,6 +5,6 @@ import checkAuth from "../middleware/authValidacion.js";
 
 const router = express.Router();
 
-router.get("/resultado", checkAuth, obtenerBalance);
+router.get("/resultado", obtenerBalance);
 
 export default router;
